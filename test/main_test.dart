e7fd488075ef3284e83ee4efc24a7ce062b9ff5f
Vitor Silva <vitor.batista@quintoandar.com.br>
2022-05-30 11:38:50 -0300

    Add render test

---
+++ test/main_test.dart
@@ -0,0 +1,19 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:startup_namer/main.dart';
+
+void main() {
+  testWidgets('Should render the app', (tester) async {
+    await tester.pumpWidget(const MyApp());
+    final titleFinder = find.text('Startup Name Generator');
+
+    expect(titleFinder, findsOneWidget);
+  });
+}
