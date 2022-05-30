c911772415bcbdbd9f8775cdaa9f32a7ea82cd7c
Vitor Silva <vitor.batista@quintoandar.com.br>
2022-05-30 18:41:33 -0300

    Add favorite screen render test

--- test/main_test.dart
+++ test/main_test.dart
@@ -5,15 +5,30 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:startup_namer/main.dart';
 
 void main() {
-  testWidgets('Should render the app', (tester) async {
-    await tester.pumpWidget(const MyApp());
-    final titleFinder = find.text('Startup Name Generator');
+  testWidgets(
+    'Should render the app',
+    (tester) async {
+      await tester.pumpWidget(const MyApp());
+      final titleFinder = find.text('Startup Name Generator');
 
-    expect(titleFinder, findsOneWidget);
-  });
+      expect(titleFinder, findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Should enter in the favorites list screen when click on the list IconButton',
+    (tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.tap(find.byType(IconButton));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Saved Suggestions'), findsOneWidget);
+    },
+  );
 }
