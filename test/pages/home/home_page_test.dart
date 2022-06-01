1465167606b944389f1278ddeeb0a567d07c1c39
Vitor Silva <vitor.batista@quintoandar.com.br>
2022-06-01 10:41:47 -0300

    test update

--- test/pages/home/home_page_test.dart
+++ test/pages/home/home_page_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -14,7 +7,7 @@ void main() {
   testWidgets(
     'Should render the app',
     (tester) async {
-      await tester.pumpWidget(const HomePage());
+      await tester.pumpWidget(const MaterialApp(home: HomePage()));
       final titleFinder = find.text('Startup Name Generator');
 
       expect(titleFinder, findsOneWidget);
@@ -24,8 +17,7 @@ void main() {
   testWidgets(
     'Should enter in the favorites list screen when click on the list IconButton',
     (tester) async {
-      await tester.pumpWidget(const HomePage());
-      await tester.pumpAndSettle();
+      await tester.pumpWidget(const MaterialApp(home: HomePage()));
 
       await tester.tap(find.byTooltip('Saved Suggestions'));
       await tester.pumpAndSettle();
